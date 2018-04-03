9dd12a142aad86bbdf4db6dbe04fce9071c92daa
Cody Nichols <canichols2@gmail.com>
2018-04-03 11:33:39 -0600

    Added initial build of DataModel

---
+++ SacramentMeeting/Models/Sacrament.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeeting.Models
+{
+    public class Sacrament
+    {
+        int Id { get; set; }
+        DateTime date { get; set; }
+
+        public virtual ICollection<Member> Speaker { get; set; }
+    }
+
+    public class Member
+    {
+        int Id { get; set; }
+        String FirstMiddleName { get; set; }
+        String LastName { get; set; }
+
+        public virtual ICollection<MemberCalling> MemberCalling { get; set; }
+    }
+    public class MemberCalling
+    {
+        int Id { get; set; }
+        int MemberId { get; set; }
+        int CallingId { get; set; }
+        bool Active { get; set; }
+
+        public virtual Member Member { get; set; }
+        public virtual Calling Calling { get; set; }
+    }
+    public class Calling
+    {
+        int Id { get; set; }
+        String CallingName { get; set; }
+
+        public virtual ICollection<MemberCalling> MemberCalling { get; set; }
+    }
+}
