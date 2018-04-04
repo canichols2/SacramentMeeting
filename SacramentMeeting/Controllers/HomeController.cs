19b1534d86bbff5f1cdaf71598934f31d72f25f2
Savannah Pyle <bea12022@byui.edu>
2018-04-03 18:50:19 -0600

    added button thang

--- SacramentMeeting/Controllers/HomeController.cs
+++ SacramentMeeting/Controllers/HomeController.cs
@@ -15,12 +15,7 @@ namespace SacramentMeeting.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
+        
 
         public IActionResult Contact()
         {
